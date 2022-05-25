855293dcd82aaf1947eb5df1e74981db209de254
Daniel Pérez <dlpcgft.com>
2022-05-25 14:53:17 +0200

    Generados "NotfoundException", "NotFOundAdvice", y "ModelAssembler" de Wine, Winery, Region y Type.
    NOTA: EL CONTENIDO DE LOS MODEL ASSEMBLER ESTA COMENTADO PARA QUE COMPILE MIENTRAS NO SE TIENEN LOS CONTROLLERS

---
+++ src/main/java/com/example/wineshop/WineModelAssembler.java
@@ -0,0 +1,19 @@
+package com.example.wineshop;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+/*@Component
+class WineModelAssembler implements RepresentationModelAssembler<Wine, EntityModel<Wine>> {
+
+    @Override
+    public EntityModel<Wine> toModel(Wine wine) {
+
+        return EntityModel.of(wine, //
+                linkTo(methodOn(WineController.class).one(wine.getId())).withSelfRel(),
+                linkTo(methodOn(WineController.class).all()).withRel("Wine List"));
+    }
+}*/
---
+++ src/main/java/com/example/wineshop/WineryModelAssembler.java
@@ -0,0 +1,19 @@
+package com.example.wineshop;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+/*@Component
+class WineryModelAssembler implements RepresentationModelAssembler<Winery, EntityModel<Winery>> {
+
+    @Override
+    public EntityModel<Winery> toModel(Winery winery) {
+
+        return EntityModel.of(winery, //
+                linkTo(methodOn(WineryController.class).one(winery.getId())).withSelfRel(),
+                linkTo(methodOn(WineryController.class).all()).withRel("Winery List"));
+    }
+}*/
